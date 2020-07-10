3d7954bae5b036ad20d44e5793521017eeebb82a
Carneir0 <anderson.hcar@gmail.com>
2020-07-10 11:13:08 -0300

    organizacao do primeiro makefile

---
+++ 01/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcoes.h"
+
+int main(int argc,char *argv[]) {
+
+    printf("There's no place like 127.0.0.1\n");
+
+    funcaoTeste();
+
+    return 0;
+}
